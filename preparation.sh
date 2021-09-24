41af1963e9be0acb4ebe5f4ed22c0b960d20aba6
luckysendouw <lucky.sendouw@gmail.com>
2021-09-24 10:29:34 +0700

    adding preparation

---
+++ preparation.sh
@@ -0,0 +1,11 @@
+
+sudo chmod 777 -R /dis-network/network.sh
+
+sudo chmod 777 -R /dis-network/scripts/configUpdate.sh
+sudo chmod 777 -R /dis-network/scripts/createChannel.sh
+sudo chmod 777 -R /dis-network/scripts/deployCC.sh
+sudo chmod 777 -R /dis-network/scripts/envVar.sh
+sudo chmod 777 -R /dis-network/scripts/setAnchorPeer.sh
+sudo chmod 777 -R /dis-network/scripts/utils.sh
+
+sudo chmod 777 -R /dis-network/organizations/ccp-generate.sh
